915ae00a08ad48bfcdb9c69f973acc2f00eea269
Tarunutukuri <tarunutukuri@gmail.com>
2026-02-11 18:48:09 +0530

    Add Phase 1 Flask backend

--- app.py
+++ app.py
@@ -1,1 +1,21 @@
+from flask import Flask, render_template, request, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/explain", methods=["POST"])
+def explain():
+    data = request.get_json()
+    word = data.get("word")
+
+    dummy_text = f"This is a dummy explanation for '{word}'."
+
+    return jsonify({
+        "explanation": dummy_text
+    })
+
+if __name__ == "__main__":
+    app.run(debug=True)
